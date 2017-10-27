a4c688b18fa0daca0ff8392205cee511f7dd50b3
zhan <390961827@qq.com>
2017-10-27 16:31:02 +0800

    完善首页

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,11 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background-color: #409EFF;
 }
+
+  body {
+    margin: 0;
+    padding: 0;
+  }
 </style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,38 +1,110 @@
 <template>
   <div>
     <el-container>
-      <el-header>
-        header
+      <el-header style="background-color: #fff; margin-bottom: 20px; height: 48px">
+        <el-row type="flex" justify="end">
+          <el-col :span="20"></el-col>
+          <el-col :span="1">登陆</el-col>
+          <el-col :span="1">注册</el-col>
+        </el-row>
       </el-header>
       <el-container>
-        <el-main>
-          <el-row v-for="blog in blog_list">
+        <!--左边栏-->
+        <el-aside width="400px"></el-aside>
+        <!--中间栏-->
+        <el-main style="padding: 0px 20px">
+          <!--发布博客部分-->
+          <el-row style="background-color: #fff; border-radius: 2px;">
             <el-col>
-              <el-row>
-                <el-col>{{blog.user_info.name}}</el-col>
-              </el-row>
-              <el-row>
-                <el-col>{{blog.created_at}}</el-col>
-              </el-row>
-              <el-row>
-                <el-col>{{blog.title}}</el-col>
-              </el-row>
-              <el-row>
-                <el-col>{{blog.content}}</el-col>
-              </el-row>
+              <el-form style="margin: 10px;">
+                <div style="text-align: left; padding: 4px; font-size: 18px; color: #1b7fb6">发布博客</div>
+                <el-form-item>
+                  <el-input type="textarea"></el-input>
+                </el-form-item>
+                <el-form-item style="text-align: right">
+                  <el-button size="mini" round>发布</el-button>
+                </el-form-item>
+              </el-form>
+            </el-col>
+          </el-row>
+          <!--博客列表-->
+          <el-row v-for="blog in blog_list" style="margin: 20px 0px">
+            <el-col>
+              <el-card :body-style="{padding: '10px'}">
+                <div slot="header">
+                  <el-row :gutter="40" align="middle">
+                    <!--头像-->
+                    <el-col :span="4">
+                      <img src="../../static/default.jpeg" height="100px" width="100px" style="border-radius: 50%">
+                    </el-col>
+                    <!--用户信息-->
+                    <el-col :span="20" style="height: 100px">
+                      <el-row>
+                        <el-col class="main-user-name">
+                          {{blog.user_info.name}}
+                        </el-col>
+                      </el-row>
+                      <el-row>
+                        <el-col class="main-blog-crated-at">
+                          {{blog.created_at}}
+                        </el-col>
+                      </el-row>
+                      <el-row>
+                        <el-col class="main-blog-title">
+                          {{blog.title}}
+                        </el-col>
+                      </el-row>
+                    </el-col>
+                  </el-row>
+                </div>
+                <!--博客正文-->
+                <div style="text-align: left">
+                  {{blog.content}}
+                </div>
+              </el-card>
             </el-col>
           </el-row>
         </el-main>
-        <el-aside width="200px">aside</el-aside>
+        <!--右边栏-->
+        <el-aside width="600px">
+          <el-row style="margin-right: 20px; width: 230px; border-radius: 2px;">
+            <el-col>
+              <div style="height: 75px; background-image: url(../../static/background.jpg)">
+                <img src="../../static/default.jpeg"
+                     style="border-radius: 50%; height: 100px; width: 100px; margin-top: 10px;">
+                <div style="color: #333; font-size: 14px;font-weight: bold;">用户姓名</div>
+              </div>
+              <div style="height: 90px; background-color: #fff"></div>
+            </el-col>
+          </el-row>
+        </el-aside>
       </el-container>
+      <el-footer>
+        footer
+      </el-footer>
     </el-container>
   </div>
 </template>
 
 <script>
   import ElCol from 'element-ui/packages/col/src/col'
+  import ElFooter from '../../node_modules/element-ui/packages/footer/src/main'
+  import ElButton from '../../node_modules/element-ui/packages/button/src/button'
+  import ElForm from '../../node_modules/element-ui/packages/form/src/form'
+  import ElFormItem from '../../node_modules/element-ui/packages/form/src/form-item'
+  import ElInput from '../../node_modules/element-ui/packages/input/src/input'
+  import ElRow from 'element-ui/packages/row/src/row'
+  import ElCard from '../../node_modules/element-ui/packages/card/src/main'
   export default {
-    components: {ElCol},
+    components: {
+      ElCard,
+      ElRow,
+      ElInput,
+      ElFormItem,
+      ElForm,
+      ElButton,
+      ElFooter,
+      ElCol},
     data () {
       return {
         blog_list: [
@@ -162,3 +234,23 @@
     }
   }
 </script>
+<style>
+.main-user-name {
+  text-align: left;
+  font-size: 14px;
+  font-weight: 700;
+  color: #333;
+  margin: 3px;
+}
+
+.main-blog-crated-at{
+  text-align: left; font-size: 12px; color: #808080;
+  margin: 3px;
+
+}
+
+.main-blog-title {
+  text-align: left; font-size: 14px; padding: 1px 0 3px; line-height: 23px; color: #333;
+  margin: 3px;
+}
+</style>
