571513a05cab3e708be57d60d1679888fce043f1
zhan <390961827@qq.com>
2017-11-07 17:54:58 +0800

    重新修改结构

--- src/App.vue
+++ src/App.vue
@@ -5,20 +5,20 @@
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  export default {
+    name: 'app'
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  background-color: #409EFF;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    background-color: #409EFF;
+  }
 
   body {
     margin: 0;
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Test from '@/components/Test'
 import Home from '@/pages/Home'
 
 Vue.use(Router)
@@ -11,11 +10,6 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
-    },
-    {
-      path: 'test',
-      name: 'Test',
-      component: Test
     }
   ]
 })
