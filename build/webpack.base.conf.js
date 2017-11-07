7ef5b4b02368668e512b5d7e88d56c6e8a4f11e1
zhan <390961827@qq.com>
2017-11-07 18:01:58 +0800

    解决编译的bug

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -45,7 +45,7 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        include: [resolve('src'), resolve('test'),resolve('/node_modules/element-ui/src'),resolve('/node_modules/element-ui/packages')]
+        include: [resolve('src'), resolve('test'),resolve('/node_modules/element-ui/src'),resolve('/node_modules/element-ui/packages'), resolve('/node_modules/_element-ui@2.0.3@element-ui/packages'), resolve('/node_modules/_element-ui@2.0.3@element-ui/src')]
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
